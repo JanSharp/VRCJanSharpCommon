c03694a3f39d7d797808578557ce8186a789754b
JanSharp <steinmeister36@gmail.com>
2025-02-02 19:50:11 +0100

    Reduce chances of memory leaks using WannaBeClass
    Specifically when using StdMove

--- Runtime/WannaBeClass.cs
+++ Runtime/WannaBeClass.cs
@@ -22,21 +22,23 @@ namespace JanSharp
 
         /// <summary>
         /// <para>Think about this like <c>std::move()</c>.</para>
-        /// <para>EXcept that we are in Udon land so what this actually does is the same thing as
-        /// <see cref="DecrementRefsCount"/>, except it does not delete the instance of this class if the
-        /// reference count dropped to 0. This means that one <b>must</b> call
-        /// <see cref="IncrementRefsCount"/> in the function the instance of this class is being passed
-        /// to.</para>
-        /// <para>Or if the receiving function does not actually keep a reference to the passed in
-        /// "r value reference" (pretend with me will you), it <b>must instead</b> call
-        /// <see cref="CheckLiveliness"/>. There is no C++ equivalent for that, this is just stupid. I know
-        /// it, you know it.</para>
-        /// <para>If it wasn't already easy to create memory leaks using <see cref="WannaBeClass"/>es then it
-        /// certainly is when using this function.</para>
+        /// <para>Except that we are in Udon land so what this actually does is the same thing as
+        /// <see cref="DecrementRefsCount"/>, except it does not immediately delete the instance of this class
+        /// if the reference count dropped to 0. This means that the function the instance of this class is
+        /// being passed to <b>must</b> call <see cref="IncrementRefsCount"/>.</para>
+        /// <para>Otherwise if the receiving function does not actually keep a reference to the passed in
+        /// "r value reference" (pretend with me will you), it can instead call <see cref="CheckLiveliness"/>.
+        /// This is optional because after 1 frame <see cref="CheckLiveliness"/> will be run automatically,
+        /// deleting the instance if reference count has dropped to 0. Therefore by calling
+        /// <see cref="CheckLiveliness"/> manually it is merely speeding up the process of instance deletion
+        /// as well as preventing obscure bugs where an instance suddenly gets deleted when it isn't
+        /// expected, because <see cref="IncrementRefsCount"/> was not called when it should have.</para>
+        /// <para>There is no C++ equivalent for that, this is just stupid. I know it, you know it.</para>
         /// </summary>
         public WannaBeClass StdMove()
         {
             referencesCount--;
+            SendCustomEventDelayedFrames(nameof(CheckLiveliness), 1);
             return this;
         }
         public void CheckLiveliness()
