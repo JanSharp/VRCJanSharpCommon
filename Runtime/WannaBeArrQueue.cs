699f7bd30b2f385f5b08a2263d6bd665bbbec7a5
JanSharp <steinmeister36@gmail.com>
2025-02-04 23:38:42 +0100

    Add WannaBeArrQueue

---
+++ Runtime/WannaBeArrQueue.cs
@@ -0,0 +1,41 @@
+
+
+namespace JanSharp
+{
+    public static class WannaBeArrQueue
+    {
+        public static void EnsureCapacity<T>(ref T[] queue, ref int startIndex, ref int count, int capacity) where T : WannaBeClass => ArrQueue.EnsureCapacity<T>(ref queue, ref startIndex, ref count, capacity);
+        public static T[] New<T>(out int startIndex, out int count) where T : WannaBeClass => ArrQueue.New<T>(out startIndex, out count);
+        public static T[] New<T>(out int startIndex, out int count, int capacity) where T : WannaBeClass => ArrQueue.New<T>(out startIndex, out count, capacity);
+
+        public static void Enqueue<T>(ref T[] queue, ref int startIndex, ref int count, T value)
+            where T : WannaBeClass
+        {
+            if (value != null)
+                value.IncrementRefsCount();
+            ArrQueue.Enqueue(ref queue, ref startIndex, ref count, value);
+        }
+
+        public static T Dequeue<T>(ref T[] queue, ref int startIndex, ref int count)
+            where T : WannaBeClass
+        {
+            T value = ArrQueue.Dequeue(ref queue, ref startIndex, ref count);
+            value.StdMove();
+            return value;
+        }
+
+        public static T Peek<T>(ref T[] queue, ref int startIndex, ref int count) where T : WannaBeClass => ArrQueue.Peek(ref queue, ref startIndex, ref count);
+
+        public static void Clear<T>(ref T[] queue, ref int startIndex, ref int count)
+            where T : WannaBeClass
+        {
+            for (int i = 0; i < count; i++)
+            {
+                T value = queue[(startIndex + i) % count];
+                if (value != null)
+                    value.DecrementRefsCount();
+            }
+            ArrQueue.Clear(ref queue, ref startIndex, ref count);
+        }
+    }
+}
