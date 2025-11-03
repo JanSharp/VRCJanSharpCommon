bcaffacd7f9e918a3d960ddab210592a25e15acf
JanSharp <steinmeister36@gmail.com>
2025-11-03 18:57:12 +0100

    Remove self reference from LocalToggleOnInteract

--- Runtime/Components/LocalToggleOnInteract.cs
+++ Runtime/Components/LocalToggleOnInteract.cs
@@ -1,14 +1,11 @@
 ﻿using UdonSharp;
 using UnityEngine;
-using VRC.SDKBase;
-using VRC.Udon;
 
 namespace JanSharp
 {
     [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
     public class LocalToggleOnInteract : UdonSharpBehaviour
     {
-        public UdonBehaviour self;
         public GameObject toToggle;
         public string activateText;
         public string deactivateText;
@@ -17,7 +14,7 @@ namespace JanSharp
         {
             bool activeSelf = !toToggle.activeSelf;
             toToggle.SetActive(activeSelf);
-            self.InteractionText = activeSelf ? deactivateText : activateText;
+            InteractionText = activeSelf ? deactivateText : activateText;
         }
     }
 }
