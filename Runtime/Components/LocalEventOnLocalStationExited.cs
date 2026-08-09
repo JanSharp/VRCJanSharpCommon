ed78affad4d9f2f9c3d5cdb0cb66e027a63b1ccd
JanSharp <steinmeister36@gmail.com>
2026-08-09 18:15:26 +0200

    Add LocalEventOnLocalStationExited

---
+++ Runtime/Components/LocalEventOnLocalStationExited.cs
@@ -0,0 +1,20 @@
+﻿using UdonSharp;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace JanSharp
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class LocalEventOnLocalStationExited : UdonSharpBehaviour
+    {
+        public UdonBehaviour target;
+        public string eventName;
+
+        public override void OnStationExited(VRCPlayerApi player)
+        {
+            if (!player.isLocal)
+                return;
+            target.SendCustomEvent(eventName);
+        }
+    }
+}
